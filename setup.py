ba9ffb0031d31fb083233d643ca57e4599b2d760
sunnyten62 <56182933+sunnyten62@users.noreply.github.com>
2020-12-13 21:20:18 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+  name='tensorrec',
+  packages=['tensorrec'],
+  version='0.26.2',
+  description='A TensorFlow recommendation algorithm and framework in Python.',
+  author='James Kirk',
+  author_email='james.f.kirk@gmail.com',
+  url='https://github.com/jfkirk/tensorrec',
+  keywords=['machine-learning', 'tensorflow', 'recommendation-system', 'python', 'recommender-system'],
+  classifiers=[],
+  install_requires=[
+      "numpy>=1.14.1",
+      "scipy>=0.19.1",
+      "six==1.11.0",
+      "tensorflow>=1.7.0",
+  ],
+)
